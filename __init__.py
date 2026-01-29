3ceab559f2645084a59a4801a4593d37b68933cc
Erik Solhaug <erik.solh@hotmail.com>
2026-01-29 00:12:42 -0600

    Initial commit: QASAP v0.5 - Interactive spectral analysis tool

---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""
+QASAP: Quick Analysis of Spectra and Profiles
+
+An interactive tool for spectral analysis, fitting, and visualization.
+"""
+
+__version__ = "0.5.0"
+__author__ = "Erik Solhaug"
+
+__all__ = ["qasap"]
---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = [line.strip() for line in fh if line.strip()]
+
+setup(
+    name="qasap",
+    version="0.5.0",
+    author="Erik Solhaug",
+    description="Quick Analysis of Spectra and Profiles - Interactive spectral analysis tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/qasap",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "qasap=qasap:main",
+        ],
+    },
+    include_package_data=True,
+    package_data={
+        "": ["*.txt", "*.key"],
+    },
+)
